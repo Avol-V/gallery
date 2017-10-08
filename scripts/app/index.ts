b0a8c647a073ed56c50e86f03ceec7006645ebd7
Andrey Avol Volynkin <mail.avol@gmail.com>
2017-10-08 18:42:41 +0300

    Add getting gallery name from page URL

--- scripts/app/index.ts
+++ scripts/app/index.ts
@@ -1,6 +1,16 @@
 import {h, render} from 'preact';
 import Root from './Root';
 
-const rootElement = h( Root, {galleryName: 'gallery'} );
+const galleryName = window.location.pathname
+	.split( '/' )
+	.filter( Boolean )
+	.pop();
+
+if ( !galleryName )
+{
+	throw new Error( 'Gallery name is not specified!' );
+}
+
+const rootElement = h( Root, {galleryName} );
 
 render( rootElement, document.body );
