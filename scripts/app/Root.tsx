ef4b084b7b9aff4b0ff498e6c8de597c93a31165
Andrey Avol Volynkin <mail.avol@gmail.com>
2017-10-08 13:51:38 +0300

    Add hotkeys

--- scripts/app/Root.tsx
+++ scripts/app/Root.tsx
@@ -58,6 +58,22 @@ class Root extends Component<RootProps, RootState>
 	}
 	
 	/**
+	 * Rendered component mounted to the DOM.
+	 */
+	public componentDidMount(): void
+	{
+		window.addEventListener( 'keydown', this.onKeyDown );
+	}
+	
+	/**
+	 * Before component will be unmounted and destroyed.
+	 */
+	public componentWillUnmount(): void
+	{
+		window.removeEventListener( 'keydown', this.onKeyDown );
+	}
+	
+	/**
 	 * Render component.
 	 */
 	public render(
@@ -100,6 +116,69 @@ class Root extends Component<RootProps, RootState>
 	private onChangeView = ( view: ViewMode ): void =>
 	{
 		this.setState( {view} );
+	}
+	
+	/**
+	 * Handle pressing keys on keyboard.
+	 */
+	private onKeyDown = ( event: KeyboardEvent ): void =>
+	{
+		if ( event.repeat )
+		{
+			return;
+		}
+		
+		const picturesCount = this.state.album.pictures.length;
+		// Default value is -1 to start from first picture when no one selected.
+		const currentPictureIndex = (
+			( this.state.currentPictureIndex == null )
+			? -1
+			: this.state.currentPictureIndex
+		);
+		
+		const changeIndex = ( index: number ): void =>
+		{
+			if (
+				!( index > picturesCount )
+				&& !( index < 0 )
+			)
+			{
+				this.onSelectPicture( index );
+			}
+		};
+		
+		const view = this.state.view;
+		
+		switch ( event.code || event.keyCode )
+		{
+			case 'ArrowRight':
+			case 39:
+			case 'Space':
+			case 32:
+				changeIndex( currentPictureIndex + 1 );
+				break;
+			
+			case 'ArrowLeft':
+			case 37:
+				changeIndex( currentPictureIndex - 1 );
+				break;
+			
+			case 'Enter':
+			case 13:
+				if ( view === ViewMode.GRID )
+				{
+					this.onChangeView( ViewMode.LOUPE );
+				}
+				break;
+			
+			case 'Escape':
+			case 27:
+				if ( view === ViewMode.LOUPE )
+				{
+					this.onChangeView( ViewMode.GRID );
+				}
+				break;
+		}
 	}
 	
 	/**
